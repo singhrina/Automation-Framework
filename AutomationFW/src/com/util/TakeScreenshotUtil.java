d8841dcf7937873dedff114060e69cd0ac3acbb6
rinasingh <rinasingh4@gmail.com>
2016-11-29 13:36:47 -0800

    new

---
+++ AutomationFW/src/com/util/TakeScreenshotUtil.java
@@ -0,0 +1,29 @@
+package com.util;  
+      
+    import java.io.File;  
+      
+    import org.apache.commons.io.FileUtils;  
+    import org.openqa.selenium.OutputType;  
+    import org.openqa.selenium.TakesScreenshot;  
+    import org.openqa.selenium.WebDriver;  
+      
+    public class TakeScreenshotUtil {  
+    	  
+    	public static void captureScreenShot(WebDriver driver,String screenShotName){  
+ 		  
+ 		try {  
+ 			File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);  
+ 			  
+ 			FileUtils.copyFile(src, new File("./Screenshots/"+screenShotName+".png"));  
+ 			  
+ 			System.out.println("screenshot taken");  
+ 		  
+ 		} catch (Exception e) {  
+ 	System.out.println("exception while taking screenshot"+e.getMessage());  
+ 	}  
+ 	}  
+   
+ }  
+
+
+
